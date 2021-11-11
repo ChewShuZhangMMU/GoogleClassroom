75f4978bdc5af2dbc648029bc8313964e73e56a0
ChewShuZhangMMU <88331426+ChewShuZhangMMU@users.noreply.github.com>
2021-11-11 21:27:26 +0800

    folder setup

---
+++ GoogleClassroomApplication/src/Main.java
@@ -0,0 +1,37 @@
+public class Main {
+}
+
+
+//user
+//create property post
+//edit property post
+//database
+
+//property
+//filter search
+
+//register
+//login
+//admin approval
+
+//add favourite
+//edit profile
+
+//User: student & organizer
+
+//classroom class
+//assignemnt class
+//to do list class
+
+//login API
+
+//calendar *special API
+
+
+
+
+
+
+
+
+
